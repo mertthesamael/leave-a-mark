3fe81c75cfca962d79945a055c133481befee8cc
Merto <mertenercan@gmail.com>
2022-08-08 19:25:20 +0300

    added form structure

--- app/src/components/Notes/Notes.js
+++ app/src/components/Notes/Notes.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import  { collection, onSnapshot } from "firebase/firestore";
+import  { collection, addDoc, onSnapshot, query, orderBy } from "firebase/firestore";
 import { db } from "../../config/firebase";
 
 
@@ -7,15 +7,17 @@ const Notes = () => {
 
 
 const [notes, setNotes] = useState([])
-
+const [inputText, setText] = useState("")
+const [inputAuthor, setAuthor] = useState("")
 
     useEffect(()=> {
 
-        const notesCollection = collection(db, 'notes');
+        const notesCollection = query(collection(db, 'notes'), orderBy("query","asc"));
         onSnapshot(notesCollection, (snapshot) => {
             setNotes( snapshot.docs.map(notes => {
 
                 return {
+                    id: notes.id,
                     ...notes.data()
                 }
   
@@ -25,13 +27,30 @@ const [notes, setNotes] = useState([])
 
     },[])
 
-
+const textHandler = (event) => {setText(event.target.value)}
+const authorHandler = (event) => {setAuthor(event.target.value)}
+const formHandler = (event) =>{event.preventDefault()
+addDoc(collection(db,'notes'), {
+    text: inputText,
+    author: inputAuthor,
+    query:9
+})
+}
     return (
         <div>
-            {notes.map(x=> <div>
-                <p>{x.text}</p> 
-                <p>{x.author}</p>
+            {notes.map(notes=>
+                <div key={notes.id}>
+                <p>{notes.text}</p> 
+                <p>{notes.author}</p>
                 </div>)}
+
+                <form onSubmit={formHandler}>
+                Text : <input onChange={textHandler} name="text" value={inputText}></input>
+                Author : <input onChange={authorHandler} name="author" value={inputAuthor}></input>
+                
+                <br/>
+                <button type="submit">Test</button>
+                </form>
         </div>
     )
 
